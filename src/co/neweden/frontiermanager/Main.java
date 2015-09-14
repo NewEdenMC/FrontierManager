b9e9491b23f9100503c0c6e49436e2e2f76ec779
aaron <aaron@neweden.co>
2015-09-14 20:51:42 +0100

    Added ability to reset world with command and permission

--- src/co/neweden/frontiermanager/Main.java
+++ src/co/neweden/frontiermanager/Main.java
@@ -11,16 +11,32 @@ import java.util.logging.Logger;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.world.WorldLoadEvent;
+import org.bukkit.event.world.WorldUnloadEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements Listener {
 	
-	public static final Logger logger = Logger.getLogger("Minecraft");
+	public final Logger logger = Logger.getLogger("Minecraft");
 	private static Set<World> worlds = new HashSet< World>();
 	
 	@Override
 	public void onEnable() {
-		this.loadWorlds();
+		getServer().getPluginManager().registerEvents(this, this);
+		loadWorlds();
+	}
+	
+	public World getWorld(String name) {
+		for (World world : worlds) {
+			if (world.getWorldName().equals(name))
+				return world;
+		}
+		return null;
 	}
 	
 	public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
@@ -33,11 +49,17 @@ public class Main extends JavaPlugin {
 				sender.sendMessage("§aSub-commands:");
 				if (sender.hasPermission("frontiermanager.addworld"))
 					sender.sendMessage("§a- addworld <worldname>§e: Adds this world to management and creates a config file");
+				if (sender.hasPermission("frontiermanager.resetworld"))
+					sender.sendMessage("§a- resetworld <worldname>§e: Resets this world imediately");
+				if (sender.hasPermission("frontiermanager.reload"))
+					sender.sendMessage("§a- reload§e: Reloads manager and data from configs");
 			} else
 				sender.sendMessage("§cYou do not have permission to run this command.");
 		} else {
 			switch (args[0]) {
 			case "addworld": addWorld(sender, args);
+				break;
+			case "resetworld": resetWorld(sender, args);
 				break;
 			case "reload": reload(sender);
 				break;
@@ -63,12 +85,33 @@ public class Main extends JavaPlugin {
 			return;
 		}
 		copyConfigFile(getResource("default_config.yml"), config);
-		World world = new World(args[1]);
+		World world = new World(this, args[1]);
 		worlds.add(world);
 		if (worlds.contains(world)) {
 			sender.sendMessage(String.format("§aConfig created and world %s now under management.", args[1]));
 		} else {
 			sender.sendMessage(String.format("§cCould not start managing world %s", args[1]));
+		}
+	}
+	
+	private void resetWorld(CommandSender sender, String args[]) {
+		if (!sender.hasPermission("frontiermanager.resetworld")) {
+			sender.sendMessage("§cYou do not have permission to run this sub-command.");
+			return;
+		}
+		if (args.length != 2 || Bukkit.getWorld(args[1]) == null) {
+			sender.sendMessage("§cYou did not specify a world or the world you specified is not loaded.");
+			return;
+		}
+		World world = getWorld(args[1]);
+		if (world == null) {
+			sender.sendMessage("§cThe world you specified is not under management.");
+			return;
+		}
+		if (world.startReset() == true) {
+			sender.sendMessage(String.format("§aThe world %s has been reset", world.getWorldName()));
+		} else {
+			sender.sendMessage(String.format("§cThe world %s could not be reset", world.getWorldName()));
 		}
 	}
 	
@@ -103,8 +146,22 @@ public class Main extends JavaPlugin {
 		logger.info(String.format("[%s] Disabled Version %s", getDescription().getName(), getDescription().getVersion()));
 	}
 	
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onWorldLoad(WorldLoadEvent event) {
+		if (loadWorld(event.getWorld().getName()) == true) {
+			logger.info(String.format("[%s] World %s now under management", getDescription().getName(), event.getWorld().getName()));
+		}
+	}
+	
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onWorldUnload(WorldUnloadEvent event) {
+		World world = getWorld(event.getWorld().getName());
+		if (world != null)
+			worlds.remove(world);
+	}
+	
 	private void loadWorlds() {
-		logger.info(String.format("[%s] Starting management for worlds", getDescription().getName()));
+		logger.info(String.format("[%s] Starting initial management for worlds", getDescription().getName()));
 		getDataFolder().mkdir();
 		File[] files;
 		try {
@@ -118,18 +175,26 @@ public class Main extends JavaPlugin {
 				continue;
 			}
 			String name = fName.substring(0, fName.length() - 4);
-			// Skip to next if this file name is not a world name
-			if (Bukkit.getWorld(name) == null) {
-				continue;
-			}
-			if (worlds.add(new World(name)) == true) {
+			if (loadWorld(name) == true)
 				logger.info(String.format("[%s] World %s now under management", getDescription().getName(), name));
-			}
 		}
 		
 		if (worlds.isEmpty()) {
 			logger.info(String.format("[%s] No worlds to manage", getDescription().getName()));
 		}
+	}
+	
+	public boolean loadWorld(String name) {
+		File configFile = new File(getDataFolder(), name + ".yml");
+		if (Bukkit.getWorld(name) == null || !configFile.exists())
+			return false;
+		FileConfiguration config = YamlConfiguration.loadConfiguration(new File(getDataFolder(), name + ".yml"));
+		if (!config.getBoolean("enabled", true))
+			return false;
+		if (worlds.add(new World(this, name)) == true)
+			return true;
+		else
+			return false;
 	}
 	
 }
--- src/co/neweden/frontiermanager/World.java
+++ src/co/neweden/frontiermanager/World.java
@@ -1,12 +1,55 @@
 package co.neweden.frontiermanager;
 
+import java.io.File;
+
+import org.bukkit.WorldCreator;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
 public class World {
 	
-	public World(String name) {
+	private Main plugin;
+	
+	public World(Main plugin, String name) {
+		this.plugin = plugin;
 		this.worldName = name;
+		this.config = YamlConfiguration.loadConfiguration(new File(plugin.getDataFolder(), worldName + ".yml"));
 	}
 	
 	private String worldName;
 	public String getWorldName() { return this.worldName; }
+	
+	private FileConfiguration config;
+	public FileConfiguration getConfig() { return this.config; }
+	
+	public boolean startReset() {
+		plugin.logger.info(String.format("[%s] Starting reset of world %s", plugin.getDescription().getName(), worldName));
+		
+		org.bukkit.World world = plugin.getServer().getWorld(worldName);
+		org.bukkit.World.Environment env = world.getEnvironment();
+		org.bukkit.generator.ChunkGenerator gen = world.getGenerator();
+		org.bukkit.WorldType type = world.getWorldType();
+		
+		File wFolder = plugin.getServer().getWorld(worldName).getWorldFolder();
+		plugin.getServer().unloadWorld(worldName, true);
+		wFolder.delete();
+		
+		WorldCreator newWorld = new WorldCreator(worldName);
+		Long seed = getConfig().getLong("seed", 0);
+		if (seed == 0) seed = (long) (Math.random() * 1000000 + 1);
+		
+		newWorld.environment(env);
+		newWorld.generator(gen);
+		newWorld.seed(seed);
+		newWorld.type(type);
+		
+		if (newWorld.createWorld() != null) {
+			plugin.logger.info(String.format("[%s] World %s has successfully been reset", plugin.getDescription().getName(), worldName));
+			return true;
+		} else {
+			plugin.logger.info(String.format("[%s] Reset of world %s has failed", plugin.getDescription().getName(), worldName));
+			return false;
+		}
+	}
 	
 }
